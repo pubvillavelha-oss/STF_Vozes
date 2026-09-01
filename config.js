8765348a5a1b7bea276321ac1d4aa3bf4de8080e
pubvillavelha-oss <pubvillavelha@gmail.com>
2026-09-01 17:24:15 -0300

    Update WEB_APP_URL to new Google Apps Script URL

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Defina a URL do Web App UMA ÚNICA VEZ aqui.
 // Todas as páginas leem esta variável via <script src="config.js">
 // ============================================================
-const WEB_APP_URL = 'COLE_AQUI_A_URL_DO_WEB_APP';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzmL1IU10IKyyjd6H48iRATaLmLWfOKnnjXFuQ70VAOn4l6zmlI5KHhenraDT24LCLzRg/exec';
