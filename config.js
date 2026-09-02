98e3d529c9246c5f6ded260f2b7577697b90db47
pubvillavelha-oss <pubvillavelha@gmail.com>
2026-09-02 17:57:34 -0300

    Fix URL formatting in IMAGENS_LATERAIS array

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const IMAGENS_LATERAIS = [
     'https://drive.google.com/thumbnail?id=1CmcEvxRKctqdcv3jDwcLQ5-vBRTk-rQK&sz=w400',
     'https://drive.google.com/thumbnail?id=1D3qVaKSK-QN_CwIbgaZf1nRo6lxPTnNk&sz=w400',
     'https://drive.google.com/thumbnail?id=11OqzpZ5NhvgmdyD2-7tCYhqc3zr6ZY_-&sz=w400',
-    'https://drive.google.com//thumbnail?id=1MNdjkYFzmHPwnsJkM9XYQWYqReYdulVt&sz=w400'
+    'https://drive.google.com/thumbnail?id=1MNdjkYFzmHPwnsJkM9XYQWYqReYdulVt&sz=w400'
 ];
