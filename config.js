b0ff88d5f168db0205d5fed3cd4f648bd3eb14e7
pubvillavelha-oss <pubvillavelha@gmail.com>
2026-09-02 00:42:19 -0300

    Add side images array to config.js
    Added a list of side images for random selection on load.

--- config.js
+++ config.js
@@ -10,3 +10,9 @@ const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzmL1IU10IKyyjd6H48
 const LOGO_URL = 'https://raw.githubusercontent.com/PUBVILLAVELHA-OSS/STF_Vozes/main/LG_STFV.png';
 // Opção 2: link direto do GitHub (descomente e use o seu)
 // const LOGO_URL = 'https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPO/main/LG_STFV.png';
+// OU lista de imagens (sorteia uma a cada carregamento)
+const IMAGENS_LATERAIS = [
+    'https://drive.google.com/file/d/1CmcEvxRKctqdcv3jDwcLQ5-vBRTk-rQK/view?usp=drive_link',
+    'https://drive.google.com/file/d/1D3qVaKSK-QN_CwIbgaZf1nRo6lxPTnNk/view?usp=drive_link',
+    'https://drive.google.com/file/d/11OqzpZ5NhvgmdyD2-7tCYhqc3zr6ZY_-/view?usp=sharing'
+];
