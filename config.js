b4b7f34e3c501734a1b178228cabaebcc729d49f
pubvillavelha-oss <pubvillavelha@gmail.com>
2026-09-08 13:56:39 -0300

    Update DRUM_URL to point to a YouTube link

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ window.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzmL1IU10IKyyjd6H4
 
 // Opcional: áudio de tambores (local ou remoto)
 // const DRUM_URL = 'https://seusite.com/audio/drumroll.mp3';
-
+const DRUM_URL = https://youtube.com/shorts/qWKvEDSjnYk?si=8pP9KskYFZqu9wiS
 
 // Caminho do logotipo oficial
 // Opção 1: arquivo local na mesma pasta
