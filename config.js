a523a0f0674dabb606b7b32a79acf58093b07fee
pubvillavelha-oss <pubvillavelha@gmail.com>
2026-09-09 17:27:55 -0300

    Update DRUM_URL to a new sound effect link

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ window.WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzmL1IU10IKyyjd6H4
 
 // Opcional: áudio de tambores (local ou remoto)
 // const DRUM_URL = 'https://seusite.com/audio/drumroll.mp3';
-const DRUM_URL = 'https://www.epidemicsound.com/pt/sound-effects/tracks/ebef5bd3-774b-4cad-bf7e-baac77ad711c/';
+const DRUM_URL = 'https://www.epidemicsound.com/sound-effects/tracks/9857eec6-77d1-4688-89ca-3415cf7d5e9d/';
 
 // Caminho do logotipo oficial
 // Opção 1: arquivo local na mesma pasta
